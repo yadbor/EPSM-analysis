4c318c3b4a1cfb70dc07299dd72e7a13d5bb45bb
yadbor <yadbor@gmail.com>
2019-10-28 12:17:45 +0800

    changed project name

--- R/clean_details.R
+++ R/clean_details.R
@@ -9,8 +9,8 @@ clean_path <- "../data"
 # paths are relative to the location of this script, so move there
 # push current location
 loc <- getwd()
-# change directory, unless alreay there 
-if (loc != "R") setwd("R")
+# change directory, unless already there 
+if (basename(loc) != "R") setwd("R")
 # now in the R directory 
 
 details <- data.table(file = dir(path = clean_path, 
@@ -20,4 +20,4 @@ details <- data.table(file = dir(path = clean_path,
 details[, year := str_match(file, "\\_(\\d{4})\\_")[,2]]
 
 # pop location back to where we were
-# setwd(loc)
+setwd(loc)
