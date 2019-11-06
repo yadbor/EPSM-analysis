e4e7a521040ca1e0677ba4c4513d53efde034e64
yadbor <yadbor@gmail.com>
2019-11-07 07:59:52 +0800

    finally realised how make_canon should work

--- data/make_canonical.R
+++ data/make_canonical.R
@@ -4,22 +4,25 @@ library(here)
 
 raw_folder <- here::here("data", "raw")
 
+# read the file supplied by Conference Company
 registrations <- fread(file.path(raw_folder, "Registration.csv"))
 meetings <- names(registrations)[-1]
 
 registrations[, prior := tstrsplit(Registration, " ")[1]]
 registrations[, prior := as.numeric(prior)]
 
-meetings <- data.table(city = c("Hobart", "Adelaide"),
-                       year = c("2017", "2018"),
-                       conference = c("EPSM", "EPSM"))
-for (m in seq.int(1, nrow(meetings))) {
-  col_ID <- meetings[m, paste(city, year)]
-  tmp <- na.omit(registrations[, .SD, .SDcols = c(col_ID, "prior")])
+#meetings <- list("Hobart 2017", "Adelaide 2018", "Perth 2019")
+canon <- list()
+for (m in meetings) {
+  tmp <- na.omit(registrations[, .SD, .SDcols = c(m, "prior")])
   setnames(tmp, c("num", "prior"))
-  DT[, .(prior = rep(tmp[, prior], tmp[, num]))]
-  DT[, c("city", "year", "conference") := meetings[m, ], by = prior]
+  DT <- data.table(prior = rep(tmp[, prior], tmp[, num]))
+  DT[, conference := "EPSM"]
+  DT[, c("city", "year") := tstrsplit(m, " "), by = prior]
+  # and fake an ID column (the rest will be NA)
+  DT[, ID := .I]
   
+  canon[[m]] <- DT
 }
 
 col_names <- c("submitter","city","state","country","theme","ID","conference","day_prior")
